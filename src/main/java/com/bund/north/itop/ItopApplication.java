7a2c16e36e3646bc096246499b3a814956c51a9c
xiongyougui <yougui.xiong@zhangmen.com>
2020-01-17 20:40:44 +0800

    init project

---
+++ src/main/java/com/bund/north/itop/ItopApplication.java
@@ -0,0 +1,13 @@
+package com.bund.north.itop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItopApplication.class, args);
+	}
+
+}
