69cd7c267de8975d9990ee30c43ff84ddacdeff3
xiongyougui <yougui.xiong@zhangmen.com>
2020-03-23 19:49:30 +0800

    主线程和子线程，默认都是非守护线程

---
+++ itop-test/src/main/java/cm/bund/north/test/jdk/thread/TestMainSubThread.java
@@ -0,0 +1,27 @@
+package cm.bund.north.test.jdk.thread;
+
+/**
+ * @author yougui.xiong
+ * @description 主线程比子线程先执行完
+ */
+public class TestMainSubThread {
+	public static void main(String[] args) {
+		System.out.println("This is main Thread start:" + Thread.currentThread().getName());
+		SubThread subThread = new SubThread();
+		subThread.start();
+		System.out.println("This is main Thread end:" + Thread.currentThread().getName());
+
+	}
+}
+
+class SubThread extends Thread {
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("This is sub Thread:" + Thread.currentThread().getName());
+	}
+}
